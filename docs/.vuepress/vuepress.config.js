b5cfdc4e0bc2c41278f99d9194b623ce2f187a67
Revolve Yao <revolveyao@outlook.com>
2022-05-06 10:13:28 +0800

    change base

--- docs/.vuepress/vuepress.config.js
+++ docs/.vuepress/vuepress.config.js
@@ -2,6 +2,7 @@ const { defaultTheme } = require('@vuepress/theme-default')
 
 const sidebarConf = require('./sidebar')
 module.exports = {
+    base:'/THR81/',
     lang: 'zh-CN',
     title: '考试必过',
     description: 'SAP SuccessFactors HXM 考试必过',

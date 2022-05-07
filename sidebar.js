573d4ad2502abcfa987cc68488556c53a7e8d20b
Revolve Yao <revolveyao@outlook.com>
2022-05-07 20:48:13 +0800

    change

--- sidebar.js
+++ sidebar.js
@@ -10,7 +10,7 @@ module.exports = [
             '/unit1/Lesson4.md',
         ],
         text: '单元2：SAP SuccessFactors HXM - 权限',
-        link: '/unit1/',
+        link: '/unit2/',
         children: [
             '/unit2/Lesson1.md',
             '/unit2/Lesson2.md',
